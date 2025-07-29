cf5faa07047addeefc84b0fbcf2d1922df41d24c
Blake0206 <blakealmanza02@gmail.com>
2025-07-29 16:59:53 -0700

    Added svgr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
 		tailwindcss(),
+		svgr(),
 		VitePWA({
 			registerType: 'prompt',
 			injectRegister: false,
