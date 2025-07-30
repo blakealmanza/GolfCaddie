1a70e424ef149fb87346806c547c24aa9e619752
Blake0206 <blakealmanza02@gmail.com>
2025-07-29 22:09:31 -0700

    Added horizontal section

--- frontend/src/components/Section.tsx
+++ frontend/src/components/Section.tsx
@@ -2,17 +2,23 @@ import type { ReactNode } from 'react';
 
 export default function Section({
 	title,
+	isHorizontal = false,
 	children,
 }: {
 	title: string;
+	isHorizontal?: boolean;
 	children: ReactNode;
 }) {
+	const classes = isHorizontal
+		? 'w-full inline-flex justify-start items-start gap-3 overflow-x-auto whitespace-nowrap'
+		: 'w-full inline-flex flex-col justify-start items-start gap-3 overflow-hidden';
+
 	return (
 		<section className='w-full inline-flex flex-col justify-start items-start gap-3 overflow-hidden'>
 			<h2 className='justify-end text-black text-base font-medium font-barlow'>
 				{title}
 			</h2>
-			{children}
+			<div className={classes}>{children}</div>
 		</section>
 	);
 }
