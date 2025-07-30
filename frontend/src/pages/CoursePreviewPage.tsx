b605401f41c06e6e0ed11f2747547ee1bfdcb52c
Blake0206 <blakealmanza02@gmail.com>
2025-07-30 16:01:37 -0700

    Set caches for all courses when fetched

--- frontend/src/pages/CoursePreviewPage.tsx
+++ frontend/src/pages/CoursePreviewPage.tsx
@@ -1,3 +1,28 @@
+import type { Course } from '@shared/types';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { useCustomAuth } from '@/context/AuthContext';
+import { fetchCourseById } from '@/context/courseService';
+
 export default function CoursePreviewPage() {
-	return <h1>This is the course preview page</h1>;
+	const { id: courseId } = useParams();
+	const { idToken } = useCustomAuth();
+	if (!courseId || !idToken) return null;
+
+	const queryClient = useQueryClient();
+
+	const { data: course } = useQuery({
+		queryKey: ['course', courseId],
+		queryFn: () => fetchCourseById(courseId, idToken),
+		initialData: () => {
+			const courses = queryClient.getQueryData(['courses']) as
+				| Course[]
+				| undefined;
+			return courses?.find((c) => c.courseId === courseId);
+		},
+		enabled: typeof courseId === 'string' && typeof idToken === 'string',
+		staleTime: 5 * 60 * 1000,
+	});
+
+	return <h1>{course?.name}</h1>;
 }
--- frontend/src/pages/CoursesPage.tsx
+++ frontend/src/pages/CoursesPage.tsx
@@ -1,4 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import type { Course } from '@shared/types';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 import ColoredButton from '@/components/ColoredButton';
 import HorizontalCard from '@/components/cards/HorizontalCard';
 import VerticalCard from '@/components/cards/VerticalCard';
@@ -11,12 +13,23 @@ export default function CoursesPage() {
 	const { idToken } = useCustomAuth();
 	if (!idToken) return null;
 
-	const { data: courses = [], isLoading } = useQuery({
-		queryKey: ['courses', idToken],
+	const queryClient = useQueryClient();
+
+	const { data: courses = [], isLoading } = useQuery<Course[]>({
+		queryKey: ['courses'],
 		queryFn: () => listCourses(idToken),
 		enabled: typeof idToken === 'string' && idToken.length > 0,
 		staleTime: 5 * 60 * 1000,
 	});
+
+	useEffect(() => {
+		if (courses.length > 0) {
+			courses.forEach((course) => {
+				queryClient.setQueryData(['course', course.courseId], course);
+				console.log(queryClient.getQueryData(['course', course.courseId]));
+			});
+		}
+	}, [courses, queryClient]);
 
 	return (
 		<>
