10c47cdf367554bdf05ec47df37362cd7fadb2d7
Blake0206 <blakealmanza02@gmail.com>
2025-07-30 19:28:01 -0700

    Templated RoundPage

--- frontend/src/pages/RoundPage.tsx
+++ frontend/src/pages/RoundPage.tsx
@@ -1,10 +1,53 @@
-import RoundContent from '../context/RoundContent';
-import { RoundProvider } from '../context/RoundContext';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import BackArrow from '@/assets/back-arrow.svg?react';
+import HoleInfoPanel from '@/components/hole/HoleInfoPanel';
+import RoundMap from '@/components/map/RoundMap';
+import { useCustomAuth } from '@/context/AuthContext';
+import { MapProvider } from '@/context/MapContext';
+import { RoundProvider, useRound } from '@/context/RoundContext';
+import { fetchRoundById } from '@/context/roundService';
+import SecondaryLayout from '@/layouts/SecondaryLayout';
 
 export default function RoundPage() {
 	return (
-		<RoundProvider>
-			<RoundContent />
-		</RoundProvider>
+		<MapProvider>
+			<RoundProvider>
+				<RoundPageContent />
+			</RoundProvider>
+		</MapProvider>
+	);
+}
+
+function RoundPageContent() {
+	const { id } = useParams();
+	const { idToken } = useCustomAuth();
+	const { dispatch } = useRound();
+
+	useEffect(() => {
+		const loadRound = async () => {
+			if (!idToken || !id) return;
+			const roundData = await fetchRoundById(id, idToken);
+			dispatch({
+				type: 'INITIALIZE_ROUND',
+				payload: { round: roundData },
+			});
+		};
+
+		loadRound();
+	}, [id, idToken, dispatch]);
+
+	return (
+		<div className='relative h-screen w-screen'>
+			<RoundMap />
+			<SecondaryLayout>
+				<div className='bg-glass rounded-lg drop-shadows border-glass blur-glass inline-flex flex-col justify-center items-center'>
+					<div className='w-10 h-10 p-0 bg-glass rounded-lg border-glass flex flex-col justify-center items-center overflow-hidden'>
+						<BackArrow className='text-black' />
+					</div>
+				</div>
+				<HoleInfoPanel />
+			</SecondaryLayout>
+		</div>
 	);
 }
