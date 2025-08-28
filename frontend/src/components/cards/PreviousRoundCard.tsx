3e4bf0d4b46d5ac6b2a470d1763f067c7d0661c4
Blake0206 <blakealmanza02@gmail.com>
2025-08-28 14:40:10 -0700

    Fixed text overflow

--- frontend/src/components/cards/PreviousRoundCard.tsx
+++ frontend/src/components/cards/PreviousRoundCard.tsx
@@ -6,28 +6,30 @@ export default function PreviousRoundCard({ roundData }: { roundData: Round }) {
 	return (
 		<Link
 			to={`/`}
-			className='self-stretch h-20 rounded-lg border-glass inline-flex justify-start items-start overflow-hidden shrink-0'
+			className='h-20 w-full rounded-lg border-glass flex overflow-hidden'
 		>
-			<img src='/chambers.png' className='w-20 h-full object-cover' />
-			<div className='flex-1 h-full  p-3 bg-glass flex justify-start items-start overflow-hidden'>
-				<div className='flex-1 self-stretch inline-flex flex-col justify-between items-start'>
-					<div className='self-stretch flex flex-col justify-start items-start gap-3'>
-						<p className='self-stretch justify-end text-black text-base font-semibold font-barlow'>
+			<img
+				src='/chambers.png'
+				alt='Chambers Bay'
+				className='w-20 h-full object-cover'
+			/>
+			<div className='flex-1 p-3 gap-3 bg-glass flex min-w-0'>
+				<div className='self-stretch flex-1 min-w-0 flex flex-col justify-between'>
+					<div className='flex flex-col gap-3'>
+						<p className='text-black text-base font-semibold font-barlow truncate'>
 							{roundData.courseName}
 						</p>
-						<p className='self-stretch justify-end text-black text-xs font-semibold font-barlow'>
+						<p className='text-black text-xs font-semibold font-barlow truncate'>
 							{roundData.courseLocation}
 						</p>
 					</div>
-					<div className='self-stretch inline-flex justify-start items-start gap-2.5'>
-						<p className='flex-1 justify-end text-black text-xs font-medium font-barlow'>
-							{new Date(roundData.startedAt).toLocaleDateString('en-US', {
-								year: 'numeric',
-								month: 'long',
-								day: 'numeric',
-							})}
-						</p>
-					</div>
+					<p className='text-black text-xs font-medium font-barlow truncate'>
+						{new Date(roundData.startedAt).toLocaleDateString('en-US', {
+							year: 'numeric',
+							month: 'long',
+							day: 'numeric',
+						})}
+					</p>
 				</div>
 				<ScoreBox score={roundData.holes[0].shots.length} />
 			</div>
