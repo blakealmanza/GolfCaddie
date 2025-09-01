6b2a03bd8a0b64ee4717237a6facbe7112f08db2
Blake0206 <blakealmanza02@gmail.com>
2025-09-01 15:42:20 -0700

    Added background image and alt text

--- frontend/src/layouts/SecondaryLayout.tsx
+++ frontend/src/layouts/SecondaryLayout.tsx
@@ -1,8 +1,25 @@
 import type { ReactNode } from 'react';
 
-export default function SecondaryLayout({ children }: { children: ReactNode }) {
+type SecondaryLayoutProps = {
+	children: ReactNode;
+	background?: {
+		src: string;
+		alt: string;
+	};
+};
+
+export default function SecondaryLayout({
+	children,
+	background,
+}: SecondaryLayoutProps) {
 	return (
-		<main className='min-h-screen w-screen px-6 py-12 inline-flex flex-col justify-between items-start overflow-hidden'>
+		<main
+			className='min-h-screen w-screen px-6 py-12 inline-flex flex-col justify-between items-start overflow-hidden bg-cover bg-center'
+			style={
+				background ? { backgroundImage: `url(${background.src})` } : undefined
+			}
+		>
+			{background?.alt && <span className='sr-only'>{background.alt}</span>}
 			{children}
 		</main>
 	);
