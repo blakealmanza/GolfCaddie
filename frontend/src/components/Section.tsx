943082435372beb0312ce4a605bc4664c63ca28a
Blake0206 <blakealmanza02@gmail.com>
2025-10-07 22:35:24 -0700

    Fixed shadow clipping

--- frontend/src/components/Section.tsx
+++ frontend/src/components/Section.tsx
@@ -11,10 +11,10 @@ export default function Section({
 }) {
 	const classes = isHorizontal
 		? 'w-full inline-flex justify-start items-start gap-3 overflow-x-auto overflow-y-hidden whitespace-nowrap'
-		: 'w-full inline-flex flex-col justify-start items-start gap-3 overflow-hidden';
+		: 'w-full inline-flex flex-col justify-start items-start gap-3';
 
 	return (
-		<section className='w-full inline-flex flex-col justify-start items-start gap-3 overflow-hidden'>
+		<section className='w-full inline-flex flex-col justify-start items-start gap-3'>
 			<h2 className='justify-end text-black text-base font-medium font-barlow'>
 				{title}
 			</h2>
